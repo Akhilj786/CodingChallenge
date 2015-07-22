052b8ec9955317679ca4358fc1589a31e175ef05
AkhilJain <jain_akhil786@yahoo.com>
2015-07-22 00:02:05 -0700

    Added few chapter:8 solutions as well.

--- Skiena/skienaMain.java
+++ Skiena/skienaMain.java
@@ -5,11 +5,15 @@ public class skienaMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Driver Class to run below Solution
+
 		solution4_4.insertPair();
 		solution4_10.insertArray();
 		solution4_31.insertArray();
 		solution4_33.insertArray();
 		solution4_34.insertArray();
+		solution4_36.insertArray();
+		solution8_9.insertArrary();
+		solution8_10.insertArrary();
 	}
 
 }
