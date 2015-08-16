4a128eb376795547d0bcf4ebe46883ac7befdfdc
AkhilJain <jain_akhil786@yahoo.com>
2015-08-16 14:32:12 -0700

    Started with a new book.

---
+++ ProgrammingElements/solution5_5.java
@@ -0,0 +1,20 @@
+package ProgrammingElements;
+
+public class solution5_5 {
+
+	// Which doors are open
+	public boolean isOpen(int i) {
+		double sqrt = Math.sqrt(i);
+		int floor_sqrt = (int) Math.floor(sqrt);
+		return (floor_sqrt * floor_sqrt == sqrt);
+	}
+
+	public void openGate(int n) {
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			if (isOpen(i))
+				result++;
+		}
+		System.out.println("Total Open Gate:" + result);
+	}
+}
