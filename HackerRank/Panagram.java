0b9342ba95fa5cc5727e27fceaeba0ae16acc2d7
Akhilj786 <jain_akhil786@yahoo.com>
2016-01-10 14:02:22 -0800

    Adding more HackerRank questions

--- HackerRank/Panagram.java
+++ HackerRank/Panagram.java
@@ -1,27 +1,31 @@
-package HackerRank;
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
 
 public class Panagram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 boolean array[]=new boolean[26];
-	        boolean flag=false;
-	        @SuppressWarnings("resource")
-			Scanner s=new Scanner(System.in);
-	        String str=s.nextLine();
-	        for(char c:str.toLowerCase().toCharArray()){
-	            if(c!=' '){
-	            int a=(int)c-97;
-	            array[a]=true;
-	            }
-	        }
-	        for(int i=0;i<26;i++){
-	            if(!array[i])
-	                flag=true;
-	        }
-	        System.out.println(flag==true?"not pangram":"pangram");
-	}
-
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        System.out.println(checkpanagram(str.toLowerCase())==true?"pangram":"not pangram");
+    }
+    
+    public static boolean checkpanagram(String s){
+    	if(s.length()<26)
+    		return false;
+    	boolean checkString[]=new boolean[26];
+    	for(int i=0;i<s.length();i++){
+    		char c=s.charAt(i);
+    		if(c>='a' && c<='z'){
+    			int k=(int)c-97;
+    			checkString[k]=true;
+    		}
+    	}
+    	for(int i=0;i<26;i++)
+    		if(!checkString[i])
+    			return false;
+    	return true;
+    }
+    
 }
